e4c7196b10f95b68f8881c81945a4654600dd998
Ben Smith <ben@smithsrus.com>
2021-06-01 19:59:44 -0400

    move shuffle test into the new shuffling_deck_spec

--- spec/card_deck_spec.rb
+++ spec/card_deck_spec.rb
@@ -48,19 +48,4 @@ describe 'CardDeck' do
     expect(card.rank).to eq unshuffled_deck_top_card
     expect(deck.cards_left).to eq 51
   end
-
-  it 'should shuffle the deck' do
-    # There is a tiny chance that a shuffled deck will equal an unshuffled one
-    deck_shuffled = deck
-    deck_shuffled.shuffle
-
-    shuffled_ranks = []
-    unshuffled_ranks = []
-    while deck.cards_left > 0
-      unshuffled_ranks << deck.deal
-      shuffled_ranks << deck_shuffled.deal
-    end
-
-    expect(shuffled_ranks).not_to eq unshuffled_ranks
-  end
 end
