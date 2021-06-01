69ab66e44bb6d384ebd6bb7ec943577eb467e247
Ben Smith <ben@smithsrus.com>
2021-06-01 15:21:04 -0400

    create 2 players when starting a game

--- lib/war_game.rb
+++ lib/war_game.rb
@@ -2,9 +2,14 @@ class WarGame
 
   def start
     @deck = CardDeck.new
+    @players = [WarPlayer.new, WarPlayer.new]
   end
 
   def deck
     @deck
+  end
+
+  def player_count
+    @players.count
   end
 end
--- lib/war_player.rb
+++ lib/war_player.rb
@@ -1,1 +1,3 @@
- 
+class WarPlayer
+  
+end
