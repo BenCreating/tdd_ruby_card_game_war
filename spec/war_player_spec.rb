87480d712246bc9944e03cca317294acea34322a
Ben Smith <ben@smithsrus.com>
2021-06-01 19:18:48 -0400

    test multiple player names

--- spec/war_player_spec.rb
+++ spec/war_player_spec.rb
@@ -3,6 +3,8 @@ require_relative '../lib/war_player'
 describe 'WarPlayer' do
   it 'create a player with the specified name' do
     player = WarPlayer.new('Alice')
+    player2 = WarPlayer.new('Bob')
     expect(player.name).to eq 'Alice'
+    expect(player2.name).to eq 'Bob'
   end
 end
