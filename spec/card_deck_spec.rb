1c3a8331abc57d7521783571ca777735f05f7f9b
Ben Smith <ben@smithsrus.com>
2021-06-01 19:47:06 -0400

    add test for specifying cards in a new deck

--- spec/card_deck_spec.rb
+++ spec/card_deck_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/card_deck'
+require_relative '../lib/playing_card'
 
 describe 'CardDeck' do
   let(:deck) { CardDeck.new }
@@ -7,7 +8,7 @@ describe 'CardDeck' do
     expect(deck.cardsLeft).to eq 52
   end
 
-  it 'should initialize the deck with all the correct cards' do
+  it 'should initialize the deck with all the default cards' do
     expected_card_ranks = %w(
       2 2 2 2
       3 3 3 3
@@ -27,6 +28,18 @@ describe 'CardDeck' do
     deck_card_ranks = []
     52.times { deck_card_ranks << deck.deal.rank }
     expect(deck_card_ranks).to match_array expected_card_ranks
+  end
+
+  it 'should initialize with specified cards' do
+    expected_ranks = %w(A 10)
+    specified_cards = []
+    expected_ranks.each { |rank| specified_cards << PlayingCard.new(rank)}
+    specified_deck = CardDeck.new(specified_cards)
+
+    deck_card_ranks = []
+    expected_ranks.count.times { deck_card_ranks << specified_deck.deal.rank }
+
+    expect(deck_card_ranks).to match_array expected_ranks
   end
 
   it 'should deal the top card' do
