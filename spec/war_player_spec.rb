dcaa6b7afe46dc06d8e15f2daf0c13cd4d348700
Ben Smith <ben@smithsrus.com>
2021-06-01 15:21:49 -0400

    test that players have names

--- spec/war_player_spec.rb
+++ spec/war_player_spec.rb
@@ -1,4 +1,8 @@
 require_relative '../lib/war_player'
 
 describe 'WarPlayer' do
+  it 'create a player with the specified name' do
+    player = WarPlayer.new('Alice')
+    expect(player.name).to eq 'Alice'
+  end
 end
