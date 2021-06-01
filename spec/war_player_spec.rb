8ef57285f5dc5f8778ce50692fbfb4cb5a8c6385
Ben Smith <ben@smithsrus.com>
2021-06-01 19:25:49 -0400

    made test descriptions more general

--- spec/war_player_spec.rb
+++ spec/war_player_spec.rb
@@ -1,12 +1,12 @@
 require_relative '../lib/war_player'
 
 describe 'WarPlayer' do
-  it 'creates a player without specifying the name' do
+  it 'creates a player without specifying attributes' do
     player = WarPlayer.new
     expect(player.name).not_to be_nil
   end
 
-  it 'creates a player with the specified name' do
+  it 'creates a player with specified attributes' do
     player = WarPlayer.new('Alice')
     player2 = WarPlayer.new('Bob')
     expect(player.name).to eq 'Alice'
