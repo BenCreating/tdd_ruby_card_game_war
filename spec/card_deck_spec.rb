73ad932ac3b11487688623f7e4362307823c3df9
Ben Smith <ben@smithsrus.com>
2021-06-01 12:32:50 -0400

    use let to define a deck variable

--- spec/card_deck_spec.rb
+++ spec/card_deck_spec.rb
@@ -1,13 +1,13 @@
 require_relative '../lib/card_deck'
 
 describe 'CardDeck' do
+  let(:deck) { CardDeck.new }
+
   it 'Should have 52 cards when created' do
-    deck = CardDeck.new
     expect(deck.cardsLeft).to eq 52
   end
 
   it 'should deal the top card' do
-    deck = CardDeck.new
     card = deck.deal
     expect(card).to_not be_nil
     expect(deck.cardsLeft).to eq 51
