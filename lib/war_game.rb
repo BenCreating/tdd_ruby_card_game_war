3f30846484378990b8d587d8c73c42633a0a08aa
Ben Smith <ben@smithsrus.com>
2021-06-01 21:34:08 -0400

    deal cards to the players at start

--- lib/war_game.rb
+++ lib/war_game.rb
@@ -5,6 +5,16 @@ class WarGame
   def start
     @deck = ShufflingDeck.new
     @players = [WarPlayer.new('Alice'), WarPlayer.new('Bob')]
+
+    @deck.shuffle
+    (@deck.cards_left/2).times do
+      card = @deck.deal
+      @players.first.pick_up_card(card)
+    end
+    @deck.cards_left.times do
+      card = @deck.deal
+      @players.last.pick_up_card(card)
+    end
   end
 
   def deck_card_count
