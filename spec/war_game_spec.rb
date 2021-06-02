0ec7c5fa1e5ce4261b3e38102138f5a54750e1fc
Ben Smith <ben@smithsrus.com>
2021-06-01 21:13:47 -0400

    test against number of cards in deck, instead of the actual cards

--- spec/war_game_spec.rb
+++ spec/war_game_spec.rb
@@ -1,15 +1,16 @@
 require_relative '../lib/war_game'
+require_relative '../lib/shuffling_deck'
 
 describe 'WarGame' do
   let(:game) { WarGame.new }
 
   context 'start' do
-    it 'creates a deck of cards' do
+    it 'creates a default deck of cards' do
       game.start
-      expect(game.deck).not_to be_nil
+      expect(game.deck_card_count).to be > 0
     end
 
-    it 'creates 2 players' do
+    it 'creates 2 default players' do
       game.start
       expect(game.player_count).to eq 2
     end
