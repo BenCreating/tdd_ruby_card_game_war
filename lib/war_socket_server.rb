f6cbebfe52dad15b2d1cc6d2b48b645e3285fd88
Ben Smith <ben@smithsrus.com>
2021-06-02 12:51:08 -0400

    store a list of clients and create a game when we have 2

--- lib/war_socket_server.rb
+++ lib/war_socket_server.rb
@@ -2,6 +2,8 @@ require 'socket'
 
 class WarSocketServer
   def initialize
+    @clients = []
+    @games = []
   end
 
   def port_number
@@ -9,7 +11,7 @@ class WarSocketServer
   end
 
   def games
-    []
+    @games
   end
 
   def start
@@ -19,11 +21,13 @@ class WarSocketServer
   def accept_new_client(player_name = "Random Player")
     client = @server.accept_nonblock
     # associate player and client
+    @clients << client
   rescue IO::WaitReadable, Errno::EINTR
     puts "No client to accept"
   end
 
   def create_game_if_possible
+    @games << WarGame.new if @clients.count == 2
   end
 
   def stop
