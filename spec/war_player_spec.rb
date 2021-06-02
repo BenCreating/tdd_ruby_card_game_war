e9477952193272875910c3d0e3f4c05445fb7601
Ben Smith <ben@smithsrus.com>
2021-06-01 20:29:47 -0400

    test for specifying a player's cards

--- spec/war_player_spec.rb
+++ spec/war_player_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/war_player'
+require_relative '../lib/playing_card'
 
 describe 'WarPlayer' do
   it 'creates a player without specifying attributes' do
@@ -8,9 +9,15 @@ describe 'WarPlayer' do
   end
 
   it 'creates a player with specified attributes' do
-    player = WarPlayer.new('Alice')
-    player2 = WarPlayer.new('Bob')
+    cards = [PlayingCard.new]
+    deck = CardDeck.new(cards)
+    player = WarPlayer.new('Alice', deck)
     expect(player.name).to eq 'Alice'
+    expect(player.card_count).to eq cards.count
+    cards2 = [PlayingCard.new, PlayingCard.new]
+    deck2 = CardDeck.new(cards2)
+    player2 = WarPlayer.new('Bob', deck2)
     expect(player2.name).to eq 'Bob'
+    expect(player2.card_count).to eq cards2.count
   end
 end
