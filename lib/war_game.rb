6902db5028cc77efb803d70b4632de4dd08d0aa0
Ben Smith <ben@smithsrus.com>
2021-06-01 21:58:55 -0400

    give cards to player 1 if they win

--- lib/war_game.rb
+++ lib/war_game.rb
@@ -24,4 +24,20 @@ class WarGame
   def players
     @players
   end
+
+  def play_round
+    player_1_card = @players.first.play_card
+    player_2_card = @players.last.play_card
+
+    if better_card(player_1_card, player_2_card) == player_1_card
+      @players.first.pick_up_card(player_1_card)
+      @players.first.pick_up_card(player_2_card)
+    end
+  end
+
+  private
+
+    def better_card(card1, card2)
+      card1
+    end
 end
