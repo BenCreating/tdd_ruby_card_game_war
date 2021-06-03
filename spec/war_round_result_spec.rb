8e05a6eef6d67123976b85d4a588f133f76b8bbe
Ben Smith <ben@smithsrus.com>
2021-06-03 18:41:08 -0400

    add test for describing a player 2 win

--- spec/war_round_result_spec.rb
+++ spec/war_round_result_spec.rb
@@ -49,4 +49,11 @@ describe 'WarRoundResult' do
     round_result = WarRoundResult.new(best_card, player_1_card, player_2_card, players)
     expect(round_result.description).to eq "Player #{player_1.name} beat #{loser_card.rank} with #{best_card.rank}"
   end
+
+  it 'sets the description, player 2 wins' do
+    player_1_card = loser_card
+    player_2_card = best_card
+    round_result = WarRoundResult.new(best_card, player_1_card, player_2_card, players)
+    expect(round_result.description).to eq "Player #{player_2.name} beat #{loser_card.rank} with #{best_card.rank}"
+  end
 end
