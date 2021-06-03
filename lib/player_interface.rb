61bd72cf0e59aa56f2f1ed0b6321d959367f22ca
Ben Smith <ben@smithsrus.com>
2021-06-02 21:28:50 -0400

    code to set ready flag

--- lib/player_interface.rb
+++ lib/player_interface.rb
@@ -3,9 +3,15 @@ require_relative 'war_player'
 class PlayerInterface
   attr_reader :client
   attr_reader :game_player
+  attr_reader :ready
 
   def initialize(client, player_name = 'Anonymous')
     @client = client
     @game_player = WarPlayer.new(player_name)
+    @ready = false
+  end
+
+  def set_ready
+    @ready = true
   end
 end
