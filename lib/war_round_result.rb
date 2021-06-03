f277d62ea317ddb6d6d5c81f32a77ae78a5c154a
Ben Smith <ben@smithsrus.com>
2021-06-03 18:48:58 -0400

    add description of a tie

--- lib/war_round_result.rb
+++ lib/war_round_result.rb
@@ -2,11 +2,11 @@ class WarRoundResult
   attr_reader :winner
   attr_reader :description
 
-  def initialize(best_card, player_1_card, player_2_card, players)
+  def initialize(best_card, player_1_card, player_2_card, players, table_cards_count)
     @player_1 = players.first
     @player_2 = players.last
     @winner = get_winner(best_card, player_1_card, player_2_card, players)
-    @description = write_description(best_card, player_1_card, player_2_card)
+    @description = write_description(best_card, player_1_card, player_2_card, table_cards_count)
   end
 
   def get_winner(best_card, player_1_card, player_2_card, players)
@@ -19,7 +19,7 @@ class WarRoundResult
     end
   end
 
-  def write_description(best_card, player_1_card, player_2_card)
+  def write_description(best_card, player_1_card, player_2_card, table_cards_count)
     if best_card == player_1_card
       loser_card = player_2_card
     elsif best_card == player_2_card
@@ -33,7 +33,7 @@ class WarRoundResult
     elsif winner == @player_2
       "Player #{winner.name} beat #{loser_card.rank} with #{best_card.rank}"
     else # the round is a tie
-
+      "Both play #{best_card.rank}! There are #{table_cards_count} cards on the table."
     end
   end
 end
