d9b82eb566db6f2e8b403e0cbc15774e3f41fb90
Ben Smith <ben@smithsrus.com>
2021-06-03 19:02:29 -0400

    refactor losing card code into method to pass test

--- lib/war_round_result.rb
+++ lib/war_round_result.rb
@@ -6,6 +6,7 @@ class WarRoundResult
     @player_1 = players.first
     @player_2 = players.last
     @winner = get_winner(best_card, player_1_card, player_2_card, players)
+    @loser_card = get_loser_card(best_card, player_1_card, player_2_card)
     @description = write_description(best_card, player_1_card, player_2_card, table_cards_count)
   end
 
@@ -19,19 +20,21 @@ class WarRoundResult
     end
   end
 
-  def write_description(best_card, player_1_card, player_2_card, table_cards_count)
-    if best_card == player_1_card
-      loser_card = player_2_card
-    elsif best_card == player_2_card
-      loser_card = player_1_card
-    else
-      loser_card = nil
+  def get_loser_card(best_card, player_1_card, player_2_card)
+    if best_card == nil
+      nil
+    elsif best_card != player_1_card
+      player_1_card
+    elsif best_card != player_2_card
+      player_2_card
     end
+  end
 
+  def write_description(best_card, player_1_card, player_2_card, table_cards_count)
     if winner == @player_1
-      "Player #{winner.name} beat #{loser_card.rank} with #{best_card.rank}"
+      "Player #{winner.name} beat #{@loser_card.rank} with #{best_card.rank}"
     elsif winner == @player_2
-      "Player #{winner.name} beat #{loser_card.rank} with #{best_card.rank}"
+      "Player #{winner.name} beat #{@loser_card.rank} with #{best_card.rank}"
     else # the round is a tie
       "Both play #{player_1_card.rank}! There are #{table_cards_count} cards on the table."
     end
