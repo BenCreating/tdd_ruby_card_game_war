155bcda9a7b5c28696c9fea65c026cd54281398c
Ben Smith <ben@smithsrus.com>
2021-06-03 10:37:08 -0400

    fix typo

--- lib/server_war_runner.rb
+++ lib/server_war_runner.rb
@@ -10,7 +10,7 @@ until server.create_game_if_possible do
   server.accept_new_client
 end
 
-game = server.game.first
+game = server.games.first
 game.start
 until game.winner do
   server.update_game(game)
