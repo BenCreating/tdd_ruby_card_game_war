93435d9ddb3207404448d06b45d226dd35258b9a
Ben Smith <ben@smithsrus.com>
2021-06-03 18:12:47 -0400

    add test for player 1 winning a war round

--- spec/war_round_result_spec.rb
+++ spec/war_round_result_spec.rb
@@ -1,5 +1,23 @@
 require_relative '../lib/war_round_result'
 
+class MockPlayingCard
+  attr_reader :rank
+  def initialize(rank)
+    @rank = rank
+  end
+end
+
 describe 'WarRoundResult' do
-  
+  let(:player_1) { 'Player 1' } # this would be a WarPlayer, but doesn't need to be for these tests
+  let(:player_2) { 'Player 2' } # this would be a WarPlayer, but doesn't need to be for these tests
+  let(:players) { [player_1, player_2] }
+  let(:best_card) { MockPlayingCard.new('10') }
+  let(:loser_card) { MockPlayingCard.new('3') }
+
+  it 'returns player 1 as the winner of the round' do
+    player_1_card = best_card
+    player_2_card = loser_card
+    round_result = WarRoundResult.new(best_card, player_1_card, player_2_card, players)
+    expect(round_result.winner).to eq player_1
+  end
 end
