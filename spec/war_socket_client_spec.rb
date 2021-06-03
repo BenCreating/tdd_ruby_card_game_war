2bde58c0b7fec3fa4496221a05a6a9e431657c01
Ben Smith <ben@smithsrus.com>
2021-06-03 14:39:23 -0400

    fix typo

---
+++ spec/war_socket_client_spec.rb
@@ -0,0 +1,32 @@
+require_relative '../lib/war_socket_client'
+
+describe WarSocketClient
+  it 'creates a client and opens a socket on the right port' do
+    client = WarSocketClient.new()
+    expect(client.socket).to eq nil
+  end
+end
+
+# class MockWarSocketClient
+#   attr_reader :socket
+#   attr_reader :output
+#
+#   def initialize(port)
+#     @socket = TCPSocket.new('localhost', port)
+#   end
+#
+#   def provide_input(text)
+#     @socket.puts(text)
+#   end
+#
+#   def capture_output(delay=0.1)
+#     sleep(delay)
+#     @output = @socket.read_nonblock(1000).chomp # not gets which blocks
+#   rescue IO::WaitReadable
+#     @output = ""
+#   end
+#
+#   def close
+#     @socket.close if @socket
+#   end
+# end
