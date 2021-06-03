edd23e51a1351ad147421ea142e16539660bf8e3
Ben Smith <ben@smithsrus.com>
2021-06-02 21:27:40 -0400

    add test for setting the ready flag

--- spec/player_interface_spec.rb
+++ spec/player_interface_spec.rb
@@ -2,11 +2,18 @@ require_relative '../lib/player_interface'
 require_relative '../lib/war_player'
 
 describe PlayerInterface do
+  let(:client) { 'client' }
+  let(:player_name) { 'Player 1' }
+
   it 'creates a player interface with the specified attributes' do
-    client = 'client'
-    player_name = 'Player 1'
     player = PlayerInterface.new(client, player_name)
     expect(player.client).to eq client
     expect(player.game_player.name).to eq player_name
+  end
+
+  it 'sets the ready flag' do
+    player = PlayerInterface.new(client, player_name)
+    player.set_ready
+    expect(player.ready).to be_true
   end
 end
