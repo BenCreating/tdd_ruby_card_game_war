f41734c63690779cebf908ef9d971333fc8c66ff
Ben Smith <ben@smithsrus.com>
2021-06-03 12:48:24 -0400

    update specs

--- spec/war_player_spec.rb
+++ spec/war_player_spec.rb
@@ -1,9 +1,11 @@
 require_relative '../lib/war_player'
 require_relative '../lib/playing_card'
+require_relative '../lib/card_deck'
 
 describe 'WarPlayer' do
+  let(:winning_hand) {  }
   it 'creates a player without specifying attributes' do
-    player = WarPlayer.new
+    player = WarPlayer.new()
     expect(player.name).not_to be_nil
     expect(player.card_count).to eq 0
   end
@@ -12,7 +14,7 @@ describe 'WarPlayer' do
     player = WarPlayer.new(name: 'Alice', cards: CardDeck.new([PlayingCard.new]))
     expect(player.name).to eq 'Alice'
     expect(player.card_count).to eq 1
-    player2 = WarPlayer.new('Bob', CardDeck.new([PlayingCard.new, PlayingCard.new]))
+    player2 = WarPlayer.new(name: 'Bob', cards: CardDeck.new([PlayingCard.new, PlayingCard.new]))
     expect(player2.name).to eq 'Bob'
     expect(player2.card_count).to eq 2
   end
@@ -27,7 +29,7 @@ describe 'WarPlayer' do
   end
 
   it 'adds a card to the hand' do
-    player = WarPlayer.new
+    player = WarPlayer.new()
     player.pick_up_card(PlayingCard.new)
     expect(player.card_count).to eq 1
   end
