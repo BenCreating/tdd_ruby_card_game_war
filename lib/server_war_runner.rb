f317c3864d9a60864b3fb1fda8d489d6397d2dae
Ben Smith <ben@smithsrus.com>
2021-06-02 21:59:53 -0400

    fill in some server_war_runner code

--- lib/server_war_runner.rb
+++ lib/server_war_runner.rb
@@ -1,6 +1,15 @@
 require_relative 'war_game'
+require_relative 'war_socket_server'
 
-game = WarGame.new
+delay = 0.5
+
+server = WarSocketServer.new
+until server.create_game_if_possible do
+  sleep(delay)
+  server.accept_new_client
+end
+
+game = server.game.first
 game.start
 until game.winner do
   puts game.play_round
