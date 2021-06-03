8aa57e5c1977f97406addb3b6daf6146285650e2
Ben Smith <ben@smithsrus.com>
2021-06-03 12:40:49 -0400

    change how war player spec passes attributes

--- spec/war_player_spec.rb
+++ spec/war_player_spec.rb
@@ -9,21 +9,19 @@ describe 'WarPlayer' do
   end
 
   it 'creates a player with specified attributes' do
-    cards = [PlayingCard.new]
-    player = WarPlayer.new('Alice', CardDeck.new(cards))
+    player = WarPlayer.new(name: 'Alice', cards: CardDeck.new([PlayingCard.new]))
     expect(player.name).to eq 'Alice'
-    expect(player.card_count).to eq cards.count
-    cards2 = [PlayingCard.new, PlayingCard.new]
-    player2 = WarPlayer.new('Bob', CardDeck.new(cards2))
+    expect(player.card_count).to eq 1
+    player2 = WarPlayer.new('Bob', CardDeck.new([PlayingCard.new, PlayingCard.new]))
     expect(player2.name).to eq 'Bob'
-    expect(player2.card_count).to eq cards2.count
+    expect(player2.card_count).to eq 2
   end
 
   it 'plays the top card from the hand' do
-    player = WarPlayer.new('Alice', CardDeck.new([PlayingCard.new('A'), PlayingCard.new('K')]))
+    player = WarPlayer.new(name: 'Alice', cards: CardDeck.new([PlayingCard.new('A'), PlayingCard.new('K')]))
     played_card = player.play_card
     expect(played_card.rank).to eq 'K'
-    player2 = WarPlayer.new('Bob', CardDeck.new([PlayingCard.new('2'), PlayingCard.new('3')]))
+    player2 = WarPlayer.new(name: 'Bob', cards: CardDeck.new([PlayingCard.new('2'), PlayingCard.new('3')]))
     played_card2 = player2.play_card
     expect(played_card2.rank).to eq '3'
   end
