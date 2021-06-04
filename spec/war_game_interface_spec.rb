77db65e740e20dd59fff2f5fe79b89156f1dcb20
Ben Smith <ben@smithsrus.com>
2021-06-03 21:44:52 -0400

    change 'client' to 'player_interface'

--- spec/war_game_interface_spec.rb
+++ spec/war_game_interface_spec.rb
@@ -1,18 +1,18 @@
 require_relative '../lib/war_game_interface'
 
 describe 'WarGameInterface' do
-  let(:client1) { 'client1' }
-  let(:client2) { 'client2' }
+  let(:player_interface_1) { 'interface 1' }
+  let(:player_interface_2) { 'interface 2' }
 
   it 'creates a new interface which generates a WarGame' do
-    game_interface = WarGameInterface.new(client1, client2)
+    game_interface = WarGameInterface.new(player_interface_1, player_interface_2)
     game_interface.game.start
     expect(game_interface.game.players.count).to eq 2
   end
 
   it 'creates a new interface and store the clients' do
-    game_interface = WarGameInterface.new(client1, client2)
-    expect(game_interface.client1).to eq client1
-    expect(game_interface.client2).to eq client2
+    game_interface = WarGameInterface.new(player_interface_1, player_interface_2)
+    expect(game_interface.player_interface_1).to eq player_interface_1
+    expect(game_interface.player_interface_2).to eq player_interface_2
   end
 end
