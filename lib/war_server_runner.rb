a989923440fbce8489e6136cae215c5c2e85bc89
Ben Smith <ben@smithsrus.com>
2021-06-04 11:59:01 -0400

    update to match new code

--- lib/war_server_runner.rb
+++ lib/war_server_runner.rb
@@ -1,5 +1,4 @@
 require_relative 'war_socket_server'
-require 'pry'
 
 delay = 0.5
 
@@ -10,12 +9,12 @@ until server.create_game_if_possible do
   server.accept_new_client
 end
 
-game = server.games.first
-until game.winner do
+game_interface = server.game_interfaces.first
+until game_interface.game.winner do
   sleep(delay)
-  server.check_ready_players(game)
-  game.update_game
+  server.check_ready_players(game_interface)
+  game_interface.update_game
 end
-puts "Winner: #{game.winner.name}"
+puts "Winner: #{game_interface.game.winner.name}"
 
 server.stop
