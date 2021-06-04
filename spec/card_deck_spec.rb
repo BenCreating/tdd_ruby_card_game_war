22fcb5e562f4bf00fb5c0388f4540869a595f369
Ben Smith <ben@smithsrus.com>
2021-06-04 08:18:29 -0400

    unwrapped array lines to meet 7 line requirement

--- spec/card_deck_spec.rb
+++ spec/card_deck_spec.rb
@@ -9,21 +9,7 @@ describe 'CardDeck' do
   end
 
   it 'should initialize the deck with all the default cards' do
-    expected_card_ranks = %w(
-      2 2 2 2
-      3 3 3 3
-      4 4 4 4
-      5 5 5 5
-      6 6 6 6
-      7 7 7 7
-      8 8 8 8
-      9 9 9 9
-      10 10 10 10
-      J J J J
-      Q Q Q Q
-      K K K K
-      A A A A
-    )
+    expected_card_ranks = %w(2 2 2 2 3 3 3 3 4 4 4 4 5 5 5 5 6 6 6 6 7 7 7 7 8 8 8 8 9 9 9 9 10 10 10 10 J J J J Q Q Q Q K K K K A A A A)
 
     deck_card_ranks = []
     52.times { deck_card_ranks << deck.deal.rank }
