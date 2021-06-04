459e4f71a6309851b87df190b4922900af62d0a5
Ben Smith <ben@smithsrus.com>
2021-06-03 21:41:46 -0400

    add test for storing the clients with a new interface

--- spec/war_game_interface_spec.rb
+++ spec/war_game_interface_spec.rb
@@ -4,9 +4,15 @@ describe 'WarGameInterface' do
   let(:client1) { 'client1' }
   let(:client2) { 'client2' }
 
-  it 'creates a new WarGame' do
+  it 'creates a new interface which generates a WarGame' do
     game_interface = WarGameInterface.new(client1, client2)
     game_interface.game.start
     expect(game_interface.game.players.count).to eq 2
+  end
+
+  it 'creates a new interface and store the clients' do
+    game_interface = WarGameInterface.new(client1, client2)
+    expect(game_interface.client1).to eq client1
+    expect(game_interface.client2).to eq client2
   end
 end
