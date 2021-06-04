4238fe54cf48800e6e491ecb19d1510a4e0eca08
Ben Smith <ben@smithsrus.com>
2021-06-03 21:45:17 -0400

    add code to store player interfaces

--- lib/war_game_interface.rb
+++ lib/war_game_interface.rb
@@ -1,8 +1,12 @@
 require_relative 'war_game'
 class WarGameInterface
   attr_reader :game
+  attr_reader :player_interface_1
+  attr_reader :player_interface_2
 
-  def initialize(client1, client2)
+  def initialize(player_interface_1, player_interface_2)
+    @player_interface_1 = player_interface_1
+    @player_interface_2 = player_interface_2
     @game = WarGame.new
   end
 end
