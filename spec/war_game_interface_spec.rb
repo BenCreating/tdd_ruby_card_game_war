71ff42bbd829b3cab84598829efe2a5153bce21c
Ben Smith <ben@smithsrus.com>
2021-06-03 22:21:01 -0400

    add spec for updating the game

--- spec/war_game_interface_spec.rb
+++ spec/war_game_interface_spec.rb
@@ -15,4 +15,12 @@ describe 'WarGameInterface' do
     expect(game_interface.player_interfaces.first).to eq player_interface_1
     expect(game_interface.player_interfaces.last).to eq player_interface_2
   end
+
+  it 'plays a round of the game' do
+    game_interface = WarGameInterface.new(player_interface_1, player_interface_2)
+    player_interface_1.set_ready
+    player_interface_2.set_ready
+    game_interface.update_game
+    expect(player_interface_1.client.capture_output).not_to eq nil
+  end
 end
