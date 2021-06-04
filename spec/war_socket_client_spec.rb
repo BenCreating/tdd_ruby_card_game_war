9b9971172ebb2197e3961a479beff6a2a783837b
Ben Smith <ben@smithsrus.com>
2021-06-04 13:56:03 -0400

    re-add war socket client and spec

--- spec/war_socket_client_spec.rb
+++ spec/war_socket_client_spec.rb
@@ -1,41 +1,24 @@
-# require_relative '../lib/war_socket_client'
-#
-# class MockWarSocketServer
-#   def port_number
-#     3336
-#   end
-#
-#   def accept_new_client
-#     @server.accept_nonblock
-#   rescue IO::WaitReadable, Errno::EINTR
-#     nil
-#   end
-#
-#   def start
-#     @server = TCPServer.new(port_number)
-#   end
-#
-#   def stop
-#     @server.close if @server
-#   end
-# end
-#
-# describe 'WarSocketClient' do
-#   before(:each) do
-#     @server = MockWarSocketServer.new
-#   end
-#
-#   after(:each) do
-#     @server.stop
-#   end
-#
-#   it 'creates a client and opens a socket on the right port' do
-#     client = WarSocketClient.new(@server.port_number)
-#     @server.accept_new_client
-#     expect(client.socket).to eq nil
-#     client.close
-#   end
-# end
+require_relative '../lib/war_socket_client'
+
+class MockWarSocketServer
+  def port_number
+    3336
+  end
+
+  def accept_new_client
+    @server.accept_nonblock
+  rescue IO::WaitReadable, Errno::EINTR
+    nil
+  end
+
+  def start
+    @server = TCPServer.new(port_number)
+  end
+
+  def stop
+    @server.close if @server
+  end
+end
 
 # class MockWarSocketClient
 #   attr_reader :socket
@@ -60,3 +43,21 @@
 #     @socket.close if @socket
 #   end
 # end
+
+describe 'WarSocketClient' do
+  before(:each) do
+    @server = MockWarSocketServer.new
+    @server.start
+  end
+
+  after(:each) do
+    @server.stop
+  end
+
+  it 'creates a client and opens a socket on the right port' do
+    client = WarSocketClient.new(@server.port_number)
+    @server.accept_new_client
+    expect(client.socket).not_to eq nil
+    client.close
+  end
+end
