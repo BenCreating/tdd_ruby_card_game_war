33700fe86fd9e7368ee1b7299b511c5d56b97419
Ben Smith <ben@smithsrus.com>
2021-06-05 16:25:13 -0400

    update tests to match refactors

--- spec/war_round_result_spec.rb
+++ spec/war_round_result_spec.rb
@@ -25,59 +25,57 @@ describe 'WarRoundResult' do
   it 'sets player 1 as the winner of the round' do
     player_1_card = best_card
     player_2_card = loser_card
-    round_result = WarRoundResult.new(best_card, player_1_card, player_2_card, players, table_cards_count)
+    round_result = WarRoundResult.new(player_1_card, player_2_card, players, table_cards_count)
     expect(round_result.winner).to eq player_1
   end
 
   it 'sets player 2 as the winner of the round' do
     player_1_card = loser_card
     player_2_card = best_card
-    round_result = WarRoundResult.new(best_card, player_1_card, player_2_card, players, table_cards_count)
+    round_result = WarRoundResult.new(player_1_card, player_2_card, players, table_cards_count)
     expect(round_result.winner).to eq player_2
   end
 
   it 'sets the winner to nil in case of a tie' do
     player_1_card = best_card
     player_2_card = best_card
-    # best_card would equal nil in this case
-    round_result = WarRoundResult.new(nil, player_1_card, player_2_card, players, table_cards_count)
+    round_result = WarRoundResult.new(player_1_card, player_2_card, players, table_cards_count)
     expect(round_result.winner).to eq nil
   end
 
   it 'sets the description, player 1 wins' do
     player_1_card = best_card
     player_2_card = loser_card
-    round_result = WarRoundResult.new(best_card, player_1_card, player_2_card, players, table_cards_count)
+    round_result = WarRoundResult.new(player_1_card, player_2_card, players, table_cards_count)
     expect(round_result.description).to eq "Player #{player_1.name} beat #{loser_card.rank} with #{best_card.rank}"
   end
 
   it 'sets the description, player 2 wins' do
     player_1_card = loser_card
     player_2_card = best_card
-    round_result = WarRoundResult.new(best_card, player_1_card, player_2_card, players, table_cards_count)
+    round_result = WarRoundResult.new(player_1_card, player_2_card, players, table_cards_count)
     expect(round_result.description).to eq "Player #{player_2.name} beat #{loser_card.rank} with #{best_card.rank}"
   end
 
   it 'sets the description, a tied round' do
     player_1_card = best_card
     player_2_card = best_card
-    # best_card would equal nil in this case
-    round_result = WarRoundResult.new(nil, player_1_card, player_2_card, players, table_cards_count)
+    round_result = WarRoundResult.new(player_1_card, player_2_card, players, table_cards_count)
     expect(round_result.description).to eq "Both play #{best_card.rank}! There are 2 cards on the table."
   end
 
   it 'returns the losing card, when the losing card is the player 1 card' do
-    round_result = WarRoundResult.new(best_card, loser_card, best_card, players, table_cards_count)
-    expect(round_result.get_loser_card(best_card, loser_card, best_card)).to eq loser_card
+    round_result = WarRoundResult.new(loser_card, best_card, players, table_cards_count)
+    expect(round_result.worse_card(best_card, loser_card, best_card)).to eq loser_card
   end
 
   it 'returns the losing card, when the losing card is the player 2 card' do
-    round_result = WarRoundResult.new(best_card, best_card, loser_card, players, table_cards_count)
-    expect(round_result.get_loser_card(best_card, best_card, loser_card)).to eq loser_card
+    round_result = WarRoundResult.new(best_card, loser_card, players, table_cards_count)
+    expect(round_result.worse_card(best_card, best_card, loser_card)).to eq loser_card
   end
 
   it 'returns nil for the losing card, when there is a tie' do
-    round_result = WarRoundResult.new(nil, best_card, best_card, players, table_cards_count)
-    expect(round_result.get_loser_card(nil, best_card, best_card)).to eq nil
+    round_result = WarRoundResult.new(best_card, best_card, players, table_cards_count)
+    expect(round_result.worse_card(nil, best_card, best_card)).to eq nil
   end
 end
