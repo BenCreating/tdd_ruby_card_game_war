8ebb72b87244edecdc0ee4e042cdf3f297dbe82e
Ben Smith <ben@smithsrus.com>
2021-06-05 21:58:52 -0400

    slight refactor and add helpful messages

--- lib/war_server_runner.rb
+++ lib/war_server_runner.rb
@@ -2,22 +2,19 @@ require_relative 'war_socket_server'
 
 delay = 0.5
 
+threads = []
+
 server = WarSocketServer.new
 server.start
+puts 'Server started'
 loop do
   sleep(delay)
   server.accept_new_client
   game_interface = server.create_game_if_possible
   if game_interface
-    # thread code goes here
+    puts 'Game created'
+    threads << Thread.new(game_interface) { game_interface.run_game }
   end
 end
 
 server.stop
-# game_interface = server.game_interfaces.first
-# until game_interface.game.winner do
-#   sleep(delay)
-#   server.check_ready_players(game_interface)
-#   game_interface.update_game
-# end
-# puts "Winner: #{game_interface.game.winner.name}"
